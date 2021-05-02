02b1914400403ab305f8763c914505238377b97a
techwizzie <sanjayipscoc@gmail.com>
2021-05-02 15:28:51 +0530

    Fixed model relationship

--- panel/models.py
+++ panel/models.py
@@ -24,37 +24,30 @@ class Member(models.Model):
         ('III year', 'III year'),
         ('IV year', 'IV year'),
     )
-    '''
-    deps  = [] 
-    departments = Department.objects.all()
-    for d in departments:
-        deps.append((d.name, d.name))
-    deps = tuple(deps)
-    '''
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
     academic_year = models.CharField(choices=years, max_length=100, null=True)
     discord_id = models.CharField(max_length=50, null=True)
     github_username = models.CharField(max_length=200, primary_key=True, default='')
-    department = models.OneToOneField(Department, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
     email = models.EmailField(null=True)
     join_date = models.DateTimeField(auto_now_add=True)
-    role = models.OneToOneField(Role, on_delete=models.CASCADE)
+    role = models.ManyToManyField(Role)
 
     def __str__(self):
         return self.first_name + self.last_name
 
 class Domain(models.Model):
     name = models.CharField(max_length=50, primary_key=True, default='')
-    coordinators = models.ManyToOneRel(Member, to='coordinators', field_name='first_name', on_delete=models.CASCADE)
-    mentors = models.ManyToOneRel(Member, to='mentors', field_name='first_name', on_delete=models.CASCADE)
+    coordinators = models.ManyToManyField(Member, related_name='coordinators')
+    mentors = models.ManyToManyField(Member, related_name='mentors')
 
     def __str__(self):
         return self.name
 
 class DomainMember(models.Model):
     member = models.OneToOneField(Member, on_delete=models.CASCADE, primary_key=True, default='')
-    domain = models.OneToOneField(Domain, on_delete=models.CASCADE)
+    domain = models.ManyToManyField(Domain)
     join_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -79,15 +72,15 @@ class Application(models.Model):
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
     email = models.EmailField(max_length=100, primary_key=True, default='')
-    department = models.OneToOneField(Department, on_delete=models.CASCADE)
-    domain = models.ManyToOneRel(Domain, to='domain', field_name='name', on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    domain = models.ManyToManyField(Domain)
     ques1 = models.TextField(max_length=400, null=True)
     writeup = models.TextField(max_length=1000, null=True)
     ac_year = models.CharField(max_length=20, null=True, choices=years)
     applied_on = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, null=True, choices=statuses)
     experience = models.TextField(max_length=500, null=True)
-    reviewer = models.OneToOneField(Member, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(Member, on_delete=models.CASCADE)
     tasksrepo = models.URLField(max_length=200, null=True)
     discord_id = models.CharField(max_length=50, null=True)
     github_username = models.CharField(max_length=200, null=True)
@@ -100,7 +93,7 @@ class Achievement(models.Model):
 
     title = models.CharField(max_length=200, null=True)
     content = models.CharField(max_length=1000, null=True)
-    achievers = models.ManyToOneRel(Member, to='achievers', field_name='first_name', on_delete=models.CASCADE)
+    achievers = models.ManyToManyField(Member)
     image = models.ImageField(null=True, upload_to='Achievements')
     date = models.DateField(primary_key=True, default='')
     post_date = models.DateTimeField(auto_now_add=True)
@@ -112,12 +105,12 @@ class Task(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200, null=True)
     goal = models.CharField(max_length=500, null=True)
-    author = models.OneToOneField(Member, on_delete=models.CASCADE)
+    author = models.ForeignKey(Member, on_delete=models.CASCADE)
     content = models.TextField(max_length=2000, null=True)
     deadline = models.DateTimeField(null=True)
     starting_time = models.DateTimeField(null=True)
     max_score = models.FloatField(null=True)
-    domain = models.OneToOneField(Domain, on_delete=models.CASCADE)
+    domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
     resource_file = models.FileField(null=True)
 
     def __str__(self):
@@ -131,7 +124,7 @@ class Submission(models.Model):
     submitted_on = models.DateTimeField(auto_now_add=True)
     submission_file = models.FileField(null=True)
     submission_text = models.CharField(max_length=500, null=True)
-    evaluator = models.OneToOneField(Member, on_delete=models.CASCADE)
+    evaluator = models.ForeignKey(Member, on_delete=models.CASCADE)
     feedback = models.CharField(max_length=500, null=True)
     
     def __str__(self):
