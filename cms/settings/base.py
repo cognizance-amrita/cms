9f56d8c98a87f7fcb3c75bf243573672d02b9458
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-05-31 11:47:18 +0530

    Updated DB

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,12 +83,20 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd7s2pjdbqamie7',
+        'USER' : 'sikglyktbqzbgq',	
+        'PASSWORD': '2e14fbfe45aab17c21dc29095f2649180fb0c76f7d2b7a87555b3771a13163ea',	
+        'HOST': 'ec2-54-243-92-68.compute-1.amazonaws.com',	
+        'PORT': '5432'
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
