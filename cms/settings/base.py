8fcbd80087f85aad21589d2726f8bfa328291cbb
Techwizzie <sanjayipscoc@gmail.com>
2022-01-22 10:50:06 +0530

    Update staticfiles

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -12,21 +12,24 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
-DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
-DISCORD_GUILD = os.environ.get('DISCORD_GUILD')
-DISCORD_CHANNEL = os.environ.get('DISCORD_CHANNEL')
+DISCORD_TOKEN = env('DISCORD_TOKEN')
+DISCORD_GUILD = env('DISCORD_GUILD')
+DISCORD_CHANNEL = env('DISCORD_CHANNEL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -167,8 +170,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
--- cms/settings/production.py
+++ cms/settings/production.py
@@ -1,5 +1,3 @@
 from .base import *
 
 DEBUG = False
-
-STATIC_ROOT = '/home/techwizzie/Open Source/cms/cms/staticfiles'
