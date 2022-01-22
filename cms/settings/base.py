c060eed4f00640bb50115e0d6333fc3d59e0f8f4
Techwizzie <sanjayipscoc@gmail.com>
2022-01-22 10:19:55 +0530

    Updated settings

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -191,7 +191,7 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
